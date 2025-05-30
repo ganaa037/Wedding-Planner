ef49161a859431869de6c58e2f977ffdfd7d46f2
24LP3863 24LP3863 <24LP3863@G302-CC0089.local>
2025-05-30 10:13:41 +0800

    mongodbteigee holboson

---
+++ backend/connectDb.js
@@ -0,0 +1,15 @@
+import { configDotenv } from "dotenv";
+import { connect } from "mongoose";
+
+configDotenv();
+const uri = process.env.MONGO_URI;
+console.log(process.env.MONGO_URI);
+
+export const connectMongoDB = async () => {
+  try {
+    await connect(uri);
+    console.log("connected to db");
+  } catch (error) {
+    console.error(error, "error");
+  }
+};
